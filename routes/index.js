f715479cc83369c52187601d8842e37114160eb8
张长悦 <dancingcodes@qq.com>
2024-07-08 11:07:41 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,7 @@ const app = require('@/app');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-// 初始化ApiDoc
-const { setApiRoute, setDocForRoutes } = require('@/config/apiDoc')
 
-// 动态引入路由文件
-const path = require('path');
-
-const routes = [{
-    fileName: 'user',
-    desc: '用户'
-}, {
-    fileName: 'driftBottle',
-    desc: '漂流瓶'
-}, {
-    fileName: 'uploadFile',
-    desc: '文件'
-}]
-
-routes.forEach(route => {
-    const filePath = path.join(__dirname, route.fileName, 'index.js')
-    app.use(`/${route.fileName}`, require(filePath));
-    setDocForRoutes(filePath, route)
-});
-
-// apiDoc
-setApiRoute(app)
+app.use('/driftBottle', require('@/routes/driftBottle'));
+app.use('/uploadFile', require('@/routes/uploadFile'));
+app.use('/user', require('@/routes/user'));

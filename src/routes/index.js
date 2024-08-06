8398c3f36e10ca4fa3df2f5e5e2391b59e925ed3
张长悦 <dancingcodes@qq.com>
2024-08-06 16:57:07 +0800

    1

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,10 @@ module.exports = (app) => {
     const bodyParser = require('body-parser');
     app.use(bodyParser.json());
 
+    app.get('/', (req, res) => {
+        res.send('<h1 style="position: absolute;left: 50%;top: 40%;transform: translate(-50%, -50%);">Moonc Applet Serve For Node.js</h1>')
+    })
+
     app.use('/user', require('@/routes/user'));
     app.use('/uploadFile', require('@/routes/uploadFile'));
     app.use('/memo', require('@/routes/memo'));

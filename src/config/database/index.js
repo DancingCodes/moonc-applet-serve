f63ac5ce56834f11154dbd6366d3364f46aa30cc
dancingcodes <dancingcodes@qq.com>
2024-09-25 09:29:50 +0800

    数据库

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const consoleLog = require('@/utils/consoleLog')
 
 // 本地
-const path = 'mongodb://MooncApplet:123456@127.0.0.1:27017/MooncApplet?authSource=MooncApplet'
+const path = 'mongodb://MooncApplet:DancingCodes1227@127.0.0.1:27017/MooncApplet?authSource=MooncApplet'
+// db.createUser({
+//     user: "MooncApplet",
+//     pwd: "DancingCodes1227",
+//     roles: [{ role: "readWrite", db: "MooncApplet" }]
+// })
 
 mongoose.connect(path).then(() => {
     consoleLog.success('数据库已连接')
